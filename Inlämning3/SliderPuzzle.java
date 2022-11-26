65afa05caf79e63a486521979c32c739bdca2668
Staz1x <fogel.stazix@gmail.com>
2022-11-26 11:09:57 +0100

    Almost done

--- Inlämning3/SliderPuzzle.java
+++ Inlämning3/SliderPuzzle.java
@@ -17,6 +17,7 @@ public class SliderPuzzle extends JFrame implements ActionListener {
     JButton nullButton = new JButton();
 
     List<JButton> buttonList = new ArrayList<>();
+    List<JButton> finishedPuzzleList = new ArrayList<>();
 
     public SliderPuzzle() {
         setLayout(new BorderLayout());
@@ -29,24 +30,52 @@ public class SliderPuzzle extends JFrame implements ActionListener {
 
         gameButton.addActionListener(this);
 
-        nullButton.setBackground(Color.WHITE);
+        nullButton.setBackground(Color.RED);
 
         for(int i = 1; i <= 15; i ++) {
             JButton button = new JButton(String.valueOf(i));
             button.setBackground(Color.WHITE);
             button.addActionListener(this);
             buttonList.add(button);
+            finishedPuzzleList.add(button);
         }
         buttonList.add(nullButton);
         for(JButton button : buttonList) {
             puzzlePanel.add(button);
+            finishedPuzzleList.add(button);
         }
 
         pack();
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
     }
+    public int findNullButton() {
+        int nullButtonPlace = 0;
+        for(JButton button : buttonList) {
+            if(button == nullButton) {
+                nullButtonPlace = buttonList.indexOf(button);
+            }
+        }
+        return nullButtonPlace;
+    }
+    public boolean canSwap(int nullButtonPlace, int clickedButtonPlace) {
+        if(nullButtonPlace%4 == clickedButtonPlace%4) {
+            if(Math.abs(nullButtonPlace/4 - clickedButtonPlace/4)==1)
+                return true;
+        }
+        if(nullButtonPlace/4 == clickedButtonPlace/4) {
+            if(Math.abs(nullButtonPlace - clickedButtonPlace) == 1)
+                return true;
+        }
+        return false;
+    }
+
+    public boolean solved (List<JButton> buttonList){
+        return buttonList.equals(finishedPuzzleList);
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -57,5 +86,18 @@ public class SliderPuzzle extends JFrame implements ActionListener {
             puzzlePanel.setLayout(new GridLayout(4,4));
             revalidate();
         }
+        else if(e.getSource() != nullButton) {
+            if(canSwap(findNullButton(), buttonList.indexOf(e.getSource()))){
+                Collections.swap(buttonList, findNullButton(), buttonList.indexOf(e.getSource()));
+                for(JButton button : buttonList) puzzlePanel.add(button);
+                puzzlePanel.setLayout(new GridLayout(4,4));
+                revalidate();
+                if(solved(buttonList)==true){
+                    JOptionPane.showMessageDialog(null, "Huzzah!");
+                }
+            }
+        }
+
     }
+
 }
